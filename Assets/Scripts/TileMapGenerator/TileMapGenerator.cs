166ca70ae8f7d8f6b6afee6d0cc25caec47037c6
Zachary W Sarette <zacharywsarette@gmail.com>
2024-03-06 22:15:04 +0900

    classroom level, first setup, change colider on player to pill 2D for smoother transition

---
+++ Assets/Scripts/TileMapGenerator/TileMapGenerator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapGenerator : MonoBehaviour
+{
+    public GameObject TileMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void generate()
+    {
+
+    }
+
+}
