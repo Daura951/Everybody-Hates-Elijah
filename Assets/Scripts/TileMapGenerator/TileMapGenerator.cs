a6c0cd3f7b4a2fdebf3563481c8ae786728effba
Zachary W Sarette <zacharywsarette@gmail.com>
2024-03-17 16:46:28 +0900

    add layers to json

--- Assets/Scripts/TileMapGenerator/TileMapGenerator.cs
+++ Assets/Scripts/TileMapGenerator/TileMapGenerator.cs
@@ -19,10 +19,6 @@ public class TileMapGenerator : MonoBehaviour
         string mapFileJson = mapFile?.text.ToString();
         LevelMap levelMap = JsonConvert.DeserializeObject<LevelMap>(mapFileJson);
 
-        // Have a huge prefab map and attach it here.
-        // It is a little dangerous, but we can throw a useful error
-        // if we are missing a prefab object
-
         int tileSize = levelMap.tileSize;
         int mapWidth = levelMap.mapWidth;
         int mapHeight = levelMap.mapHeight;
@@ -32,9 +28,6 @@ public class TileMapGenerator : MonoBehaviour
             UnityEngine.Debug.Log("Layer Name: " + layer.name);
             foreach (GameObjectPosition gameObjectPosition in layer.gameObjectPositions)
             {
-                UnityEngine.Debug.Log("x: " + gameObjectPosition.x);
-                UnityEngine.Debug.Log("y: " + gameObjectPosition.y);
-                UnityEngine.Debug.Log("Game Object: " + gameObjectPosition.gameObjectName);
                 GameObject obj = tileGameObjectDict.getGameObject(gameObjectPosition.gameObjectName); 
                 Instantiate(obj, new Vector3(gameObjectPosition.x, gameObjectPosition.y, 0f), Quaternion.identity);
             }
